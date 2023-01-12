e327e96fda79ba763ff431d2d5a7f35e964b5218
Brian Liu <liub@salinehornets.org>
2023-01-11 19:55:46 -0500

    Added constants to the constant class and filled in methods in the SwerveSubsystem class

--- src/main/java/frc/robot/SwerveSubsystem.java
+++ src/main/java/frc/robot/SwerveSubsystem.java
@@ -1,5 +1,10 @@
 package frc.robot;
 
+import java.util.Dictionary;
+import java.util.Hashtable;
+
+import com.ctre.phoenixpro.hardware.Pigeon2;
+
 /*
  * This class provides functions to drive at a given angle and direction,
  * and performs the calculations required to achieve that
@@ -10,13 +15,21 @@ public class SwerveSubsystem {
      * all of our SwerveModules
      */
 
+    private Dictionary<String, SwerveModule> swerveModules = new Hashtable<String, SwerveModule>();
+    private Pigeon2 gyro;
+
     /*
      * This constructor should create an instance of the pidgeon class, and should construct four copies of the
      * SwerveModule class and add them to our SwerveModule dictionary
      * Use values from the Constants.java class
      */
     public SwerveSubsystem() {
+        gyro = new Pigeon2(Constants.GYRO_CANCODER_ID, Constants.CANBUS);
 
+        swerveModules.put("FL", new SwerveModule(Constants.FL_Motor_ID, Constants.FL_ANGLE_ID));
+        swerveModules.put("FR", new SwerveModule(Constants.FR_Motor_ID, Constants.FR_ANGLE_ID));
+        swerveModules.put("BL", new SwerveModule(Constants.BL_Motor_ID, Constants.BL_ANGLE_ID));
+        swerveModules.put("BR", new SwerveModule(Constants.BR_Motor_ID, Constants.BR_ANGLE_ID));
     }
 
     public class SwerveRequest {
@@ -36,6 +49,10 @@ public class SwerveSubsystem {
      * This function returns the angle (in radians) of the robot based on the value from the pidgeon 2.0
      */
     public double getRobotAngle() {
-        throw new UnsupportedOperationException();
+        return (gyro.getAngle() * Math.PI) / 180;
+    }
+
+    public void resetGyro() {
+        gyro.reset();
     }
 }
