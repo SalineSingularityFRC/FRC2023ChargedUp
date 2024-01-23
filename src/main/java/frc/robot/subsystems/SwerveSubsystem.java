8e5c3d2bd50aa461d5abe485aaeb8ee3f3393998
BillHongFirstRobotics <hongw@salinehornets.org>
2024-01-22 20:17:13 -0500

    Fixed PID Values for PathPlanner

--- src/main/java/frc/robot/subsystems/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/SwerveSubsystem.java
@@ -132,8 +132,8 @@ public class SwerveSubsystem implements Subsystem {
         supplier_chasis, // ChassisSpeeds supplier. MUST BE ROBOT RELATIVE
         consumer_chasis, // Method that will drive the robot given ROBOT RELATIVE ChassisSpeeds
         new HolonomicPathFollowerConfig( // HolonomicPathFollowerConfig, this should likely live in your Constants class
-            new PIDConstants(5.0, 0.0, 0.0), // Translation PID constants
-            new PIDConstants(5.0, 0.0, 0.0), // Rotation PID constants
+            new PIDConstants(0.0001, 0.0, 0.0), // Translation PID constants
+            new PIDConstants(0.0001, 0.0, 0.0), // Rotation PID constants
             4.5, // Max module speed, in m/s
             0.70832, // Drive base radius in meters. Distance from robot center to furthest module.
             new ReplanningConfig() // Default path replanning config. See the API for the options here
